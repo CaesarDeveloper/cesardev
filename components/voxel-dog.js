5da212b78cc3253ca52c5af98c14d9661570a8a8
Caesar Developer <caesararg.dev@gmail.com>
2024-03-29 09:36:26 -0600

    Fix minor issues

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -90,14 +90,14 @@ const VoxelDog = () => {
         frame = frame <= 100 ? frame + 1 : frame
 
         if (frame <= 100) {
-          // const p = initialCameraPosition
-          // const rotSpeed = -easeOutCirc(frame / 120) * Math.PI * 20
+          const p = initialCameraPosition
+          const rotSpeed = -easeOutCirc(frame / 120) * Math.PI * 20
 
-          // camera.position.y = 10
-          // camera.position.x =
-          //   p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed)
-          // camera.position.z =
-          //   p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed)
+          camera.position.y = 10
+          camera.position.x =
+            p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed)
+          camera.position.z =
+            p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed)
           camera.lookAt(target)
         } else {
           controls.update()
