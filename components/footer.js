e937880f430af27d9acdf6307559f39be805d1ff
Caesar Developer <caesararg.dev@gmail.com>
2024-04-25 14:25:50 -0600

    Fix minor issues

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} Inspired by <a href="https://www.craftz.dog/"> Takuya Matsuyama's </a>
+      &copy; {new Date().getFullYear()} Inspired by <a href="https://www.craftz.dog/"> Takuya Matsuyama&apos;s </a>
       personal portfolio design.
     </Box>
   )
